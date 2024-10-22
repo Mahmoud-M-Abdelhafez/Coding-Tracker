f082fe6168828db8526b70bb9c700f81641f40a1
Kareem-Mohamed0 <KAREEM.mohamed3321@el-eng.menofia.edu.eg>
2024-10-23 01:53:11 +0300

    ProblemSolving file is Added

---
+++ Coding Tracker/IProplemSolving.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Tracker
+{
+    internal interface IProplemSolving
+    {
+        public string Concat(string s1,string s2);
+        public int[] ReverseArr(int[] arr);
+    }
+}
---
+++ Coding Tracker/ProplemSolving.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Tracker
+{
+    internal class ProplemSolving : IProplemSolving
+    {
+        public string Concat(string s1, string s2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] ReverseArr(int[] arr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
