3ef5bf0028768bd7115d590ac1330006a2938c57
Kareem-Mohamed0 <KAREEM.mohamed3321@el-eng.menofia.edu.eg>
2024-10-23 00:16:18 +0300

    implement Funcs of the Square

--- Coding Tracker/squre.cs
+++ Coding Tracker/squre.cs
@@ -9,5 +9,16 @@ namespace Coding_Tracker
     internal class squre : ISqure
     {
         /* write functions to calculate area and perimeter for squre*/
+        public double Calc_Area(float length, float width)
+        {
+            double area = length * width;
+            return area;
+        }
+
+        public double Calc_Perimeter(float length, float width)
+        {
+            double perimeter = length + width;
+            return perimeter;
+        }
     }
 }
