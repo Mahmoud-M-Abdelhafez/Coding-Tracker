ff903baf1326f3ad2d2cc4fe0789d797b061cef3
Mahmoud Abdelhafez <mahmoud.abdelhafz249@gmail.com>
2024-10-23 01:14:41 +0300

    implement Triangle Functions

---
+++ Coding Tracker/CalcFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Tracker
+{
+    internal class CalcFunctions : ICalcFunctions
+    {
+        public double TriangleArea(double Base, double height)
+        {
+            return(((0.5)*Base) + height);
+        }
+
+        public double TrianglePerimeter(double x, double y, double z)
+        {
+            return(x+y+z);
+        }
+    }
+}
--- Coding Tracker/Program.cs
+++ Coding Tracker/Program.cs
@@ -4,10 +4,30 @@
     {
         static void Main(string[] args)
         {
-                /*Kemo Code*/
+            /*Kemo Code*/
 
 
-                /*Mahmoud Code*/
+            /*Mahmoud Code*/
+
+            double Base = 0, height = 0 , area = 0 , perimeter=0;
+            Console.WriteLine("please, Enter Base of Triangle : ");
+            Base = float.Parse(Console.ReadLine());
+            Console.WriteLine("please, Enter height of Triangle : ");
+            height = float.Parse(Console.ReadLine());
+            CalcFunctions calcFunctions = new CalcFunctions();
+            area= calcFunctions.TriangleArea(Base, height);
+            Console.WriteLine(area);
+            double X=0 , y=0 , Z=0 ;
+            Console.WriteLine("please, Enter Length of X : ");
+            X = float.Parse(Console.ReadLine());
+            Console.WriteLine("please, Enter Length of Y : ");
+            y = float.Parse(Console.ReadLine());
+            Console.WriteLine("please, Enter Length of Z : ");
+            Z = float.Parse(Console.ReadLine());
+            perimeter = calcFunctions.TrianglePerimeter(X, y, Z);
+            Console.WriteLine(perimeter);
+
+
         }
     }
 }
