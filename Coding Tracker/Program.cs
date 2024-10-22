45a3808a8b10b2e7c428783c38e4bd529d50abe5
Mahmoud Abdelhafez <mahmoud.abdelhafz249@gmail.com>
2024-10-23 01:55:21 +0300

    Update Program.cs

--- Coding Tracker/Program.cs
+++ Coding Tracker/Program.cs
@@ -5,53 +5,89 @@ namespace Coding_Tracker
     {
         static void Main(string[] args)
         {
-            /*Kemo Code*/
-<<<<<<< kareem-Branch
-            Console.WriteLine("Select:\n 1.CalcArea \n 2.CalcPerimeter");
-            var selected = int.Parse(Console.ReadLine());
-            var squre = new Squre();
-            float width;
-            float length;
-            float result = 0;
-            switch (selected)
+            bool Run=true;
+            while (Run)
             {
-                case 1:
-                    Console.WriteLine("Enter the width and length:");
-                    width = float.Parse(Console.ReadLine());
-                    length = float.Parse(Console.ReadLine());
-                    result = squre.Calc_Area(length, width);
-                    break;
-                case 2:
-                    Console.WriteLine("Enter the length and width");
-                    length = float.Parse(Console.ReadLine());
-                    width = float.Parse(Console.ReadLine());
-                    result = squre.Calc_Perimeter(length, width);
-                    break;
+                Console.WriteLine("Select Shape:\n 1.Square \n 2.Triangle \n 3.Exit");
+                var Option = int.Parse(Console.ReadLine());
+
+                switch (Option)
+                {
+                    case 1:
+                        Console.WriteLine("Select:\n 1.CalcArea \n 2.CalcPerimeter");
+                        var selected = int.Parse(Console.ReadLine());
+                        var squre = new Squre();
+                        float width;
+                        float length;
+                        float result = 0;
+                        switch (selected)
+                        {
+                            case 1:
+                                Console.WriteLine("Enter the width and length:");
+                                width = float.Parse(Console.ReadLine());
+                                length = float.Parse(Console.ReadLine());
+                                result = squre.Calc_Area(length, width);
+                                Console.WriteLine($"Area of Square = {result}");
+                                break;
+                            case 2:
+                                Console.WriteLine("Enter the length and width");
+                                length = float.Parse(Console.ReadLine());
+                                width = float.Parse(Console.ReadLine());
+                                result = squre.Calc_Perimeter(length, width);
+                                Console.WriteLine($"Perimeter of Square = {result}");
+                                break;
+                            default:
+                                Console.WriteLine("ERROR, Please Enter 1 or 2 !!!!");
+                                break;
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Select:\n 1.CalcArea \n 2.CalcPerimeter");
+                        var Select = int.Parse(Console.ReadLine());
+                        CalcFunctions calcFunctions = new CalcFunctions();
+                        double Base = 0, height = 0, area = 0, perimeter = 0;
+                        switch (Select)
+                        {
+                            case 1:
+                                Console.WriteLine("please, Enter Base of Triangle : ");
+                                Base = float.Parse(Console.ReadLine());
+                                Console.WriteLine("please, Enter height of Triangle : ");
+                                height = float.Parse(Console.ReadLine());
+                                area = calcFunctions.TriangleArea(Base, height);
+                                Console.WriteLine($"Area of Triangle = {area}");
+                                break;
+                            case 2:
+                                double X = 0, y = 0, Z = 0;
+                                Console.WriteLine("please, Enter Length of X , Y , Z : ");
+                                X = float.Parse(Console.ReadLine());
+                                y = float.Parse(Console.ReadLine());
+                                Z = float.Parse(Console.ReadLine());
+                                perimeter = calcFunctions.TrianglePerimeter(X, y, Z);
+                                Console.WriteLine($"Perimeter of Triangle = {perimeter}");
+                                break;
+                            default:
+                                Console.WriteLine("ERROR, Please Enter 1 or 2 !!!!");
+                                break;
+                        }
+                        break;
+
+                    case 3:
+                        Run = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("ERROR, Please Enter 1 or 2 !!!!");
+                        break;
+                }
             }
-            Console.WriteLine($"The result is equal to:{result}");
+            
 
 
+            
+
+            
 
 
-            /*Mahmoud Code*/
-
-            double Base = 0, height = 0 , area = 0 , perimeter=0;
-            Console.WriteLine("please, Enter Base of Triangle : ");
-            Base = float.Parse(Console.ReadLine());
-            Console.WriteLine("please, Enter height of Triangle : ");
-            height = float.Parse(Console.ReadLine());
-            CalcFunctions calcFunctions = new CalcFunctions();
-            area= calcFunctions.TriangleArea(Base, height);
-            Console.WriteLine(area);
-            double X=0 , y=0 , Z=0 ;
-            Console.WriteLine("please, Enter Length of X : ");
-            X = float.Parse(Console.ReadLine());
-            Console.WriteLine("please, Enter Length of Y : ");
-            y = float.Parse(Console.ReadLine());
-            Console.WriteLine("please, Enter Length of Z : ");
-            Z = float.Parse(Console.ReadLine());
-            perimeter = calcFunctions.TrianglePerimeter(X, y, Z);
-            Console.WriteLine(perimeter);
 
 
 
