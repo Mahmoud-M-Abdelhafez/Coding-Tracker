d936dd576991129a02d3364618f30167d34fc5c1
Mahmoud Abdelhafez <mahmoud.abdelhafz249@gmail.com>
2024-10-22 21:40:54 +0300

    write functions to calculate area and perimeter for squre

---
+++ Coding Tracker/ISqure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Tracker
+{
+    internal interface ISqure
+    {
+        public double Calc_Area(float length , float width);
+        public double Calc_Perimeter(float length, float width);
+    }
+}
--- Coding Tracker/Program.cs
+++ Coding Tracker/Program.cs
@@ -4,10 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello,Mahmoud!");
-            Console.WriteLine("Hello,kemo!");
-            /********************* Kareem Clone ***********************/
-            Console.WriteLine("Hello From Kareem Salama, I made a clone From your Repository");
+                /*Kemo Code*/
+
+
+                /*Mahmoud Code*/
         }
     }
 }
