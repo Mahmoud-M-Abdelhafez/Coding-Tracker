5c546d61ece8f42aa48438c797eb60603d8b9dbd
Kareem-Mohamed0 <KAREEM.mohamed3321@el-eng.menofia.edu.eg>
2024-10-23 00:09:46 +0300

    CalcFunctions

---
+++ Coding Tracker/ICalcFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding_Tracker
+{
+    public interface ICalcFunctions
+    {
+        public double TriangleArea(double Base , double height);
+        public double TrianglePerimeter(double x, double y ,double z);
+    }
+}
